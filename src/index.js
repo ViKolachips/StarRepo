b1e126118dbfeab0bbd8d70407d1edaf5c485606
ViKolachips <vdechalambert1510@gmail.com>
2019-01-17 16:36:00 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './css/style.styl'
+
+console.log('Hello webpack')
+
+// Hot
+if(module.hot)
+{
+    module.hot.accept()
+
+    module.hot.dispose(() =>
+    {
+        console.log('dispose')
+
+        application.destructor()
+
+        document.body.removeChild($image)
+    })
+}
